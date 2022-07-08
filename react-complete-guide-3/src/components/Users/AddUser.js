8a8a24205b54458e5bfbf3961d45d941211ccf5d
uchario <uchario@yahoo.com>
2022-07-08 08:25:57 +0100

    added AddUser.modules.css

--- react-complete-guide-3/src/components/Users/AddUser.js
+++ react-complete-guide-3/src/components/Users/AddUser.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Card from '../UI/Card';
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
     return (
-        <form>
-            <label>Username</label>
-            <input id="username" type="text"/>
-            <label>Age</label>
-            <input id="age" type="number"/>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+
     )
 }
 
